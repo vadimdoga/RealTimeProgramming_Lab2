aa4e8a7dc743d234e842ae0cebf55932cb8c7bd3
vadimdoga <vadim.doga1@gmail.com>
2020-05-08 11:17:38 +0300

    add unsubscribe to pub.sub

--- lib/Subscriber.ex
+++ lib/Subscriber.ex
@@ -4,16 +4,13 @@ defmodule Subscriber do
   def start_link() do
     GenServer.start_link(__MODULE__, %{})
 
-    IO.inspect(self())
     {:ok, self()}
   end
 
   @impl true
   def init(_init_arg) do
-    port = 8681
-    {:ok, subscriber_socket} = :gen_udp.open(port, [:binary, {:active, false}])
 
-    {:ok, %{:subscriber_socket => subscriber_socket, :subscriber_port => port}}
+    {:ok, %{}}
   end
 
   # def unsubscribe(address) do
@@ -23,16 +20,24 @@ defmodule Subscriber do
   # end
 
   @impl true
-  def handle_cast({:subscribe, topic}, state) do
+  def handle_cast({:unsubscribe, [port, topic]}, state) do
+    subscription_manager = :global.whereis_name('subscription_manager')
+    SubscriptionManager.delete(subscription_manager, port)
+    recv_pid = state[:recv_pid]
+    Process.exit(recv_pid, :unsubscribe)
+    IO.puts "Address unsubscribed succesfull from topic -  #{topic}"
 
-    subscriber_port = state[:subscriber_port]
-    subscriber_socket = state[:subscriber_socket]
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_cast({:subscribe, [topic, subscriber_port, subscriber_socket]}, state) do
     subscription_manager = :global.whereis_name('subscription_manager')
     SubscriptionManager.put(subscription_manager, subscriber_port, topic)
     IO.puts "Address subscribed succesfull on topic -  #{topic}"
 
-    spawn(__MODULE__, :recv_from_subscription, [subscriber_socket, topic])
-
+    recv_pid = spawn(__MODULE__, :recv_from_subscription, [subscriber_socket, topic])
+    state = Map.put(state, :recv_pid, recv_pid)
     {:noreply, state}
   end
 
--- lib/test_subscriber.ex
+++ lib/test_subscriber.ex
@@ -7,8 +7,14 @@ defmodule TestSubscriber do
 
   @impl true
   def init(init_arg) do
+    port = 8681
+    topic = :iot
+    {:ok, subscriber_socket} = :gen_udp.open(port, [:binary, {:active, false}])
+
     {:ok, subscriber_pid} = GenServer.start_link(Subscriber, [])
-    GenServer.cast(subscriber_pid, {:subscribe, :iot})
+    GenServer.cast(subscriber_pid, {:subscribe, [topic, port, subscriber_socket]})
+    :timer.sleep(10000)
+    GenServer.cast(subscriber_pid, {:unsubscribe, [port, topic]})
 
     {:ok, init_arg}
   end
