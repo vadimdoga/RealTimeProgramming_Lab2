78e31a707b32d4de15ab45ff0c474a95d15b7ce3
vadimdoga <vadim.doga1@gmail.com>
2020-05-10 11:19:01 +0300

    refactored join

--- lib/join_sensors.ex
+++ lib/join_sensors.ex
@@ -9,7 +9,6 @@ defmodule JoinSensors do
   def init(init_arg) do
     port = 8681
     topic = :join
-    list = []
     list_iot = []
     list_sensors = []
     list_legacy_sensors = []
@@ -18,14 +17,16 @@ defmodule JoinSensors do
 
     {:ok, subscriber_pid} = GenServer.start_link(Subscriber, [])
 
-    recv_subscriber_pid = spawn_link(__MODULE__, :recv_from_subscription, [subscriber_socket, topic, list, list_iot, list_sensors, list_legacy_sensors])
+    recv_subscriber_pid = spawn_link(__MODULE__, :recv_from_subscription, [subscriber_socket, list_iot, list_sensors, list_legacy_sensors])
 
     GenServer.cast(subscriber_pid, {:subscribe, [topic, port, recv_subscriber_pid]})
 
     {:ok, init_arg}
   end
 
-  def recv_from_subscription(subscriber_socket, _topic, list, list_iot, list_sensors, list_legacy_sensors) do
+  def recv_from_subscription(subscriber_socket, list_iot, list_sensors, list_legacy_sensors) do
+    joined_list = []
+
     recv = :gen_udp.recv(subscriber_socket, 0)
     recv = get_recv_data(recv)
     map = convert_string_to_map(recv)
@@ -34,47 +35,27 @@ defmodule JoinSensors do
     topic = classify_map(map)
     {list_iot, list_sensors, list_legacy_sensors} = add_to_list(list_iot, list_sensors, list_legacy_sensors, topic, map)
 
-    {list, list_iot, list_legacy_sensors, list_sensors} = if Enum.count(list_iot) > 1 && Enum.count(list_sensors) > 1 && Enum.count(list_legacy_sensors) > 1 do
-      get_duplicates(list, list_iot, list_legacy_sensors, list_sensors)
+    {joined_list, list_iot, list_legacy_sensors, list_sensors} = if Enum.count(list_iot) > 1 && Enum.count(list_sensors) > 1 && Enum.count(list_legacy_sensors) > 1 do
+      join_sensors(list_iot, list_legacy_sensors, list_sensors)
     else
-      {list, list_iot, list_legacy_sensors, list_sensors}
+      {joined_list, list_iot, list_legacy_sensors, list_sensors}
     end
 
-    list_size = Enum.count(list)
-    {list, list_iot, list_legacy_sensors, list_sensors} = if list_size == 100 do
-      join_and_publish(list)
-      {[], [], [], []}
-    else
-      {list, list_iot, list_legacy_sensors, list_sensors}
+    if joined_list != [] do
+      publish_list(joined_list)
     end
-    recv_from_subscription(subscriber_socket, topic, list, list_iot, list_sensors, list_legacy_sensors)
+
+    recv_from_subscription(subscriber_socket, list_iot, list_sensors, list_legacy_sensors)
   end
 
-  defp join_and_publish(_list) do
-    IO.puts "Its time to send"
-    # topic = :forecast
+  defp publish_list(joined_list) do
+    topic = :forecast
+    publisher_pid = :global.whereis_name('publisher_pid')
 
-    # timestamp_list = Enum.reduce(list, [], fn x, acc ->
-    #   [ String.to_integer(x["unix_timestamp_100us"]) | acc]
-    # end)
-    # timestamp_list = Enum.sort(timestamp_list)
-    # timestamp_list = duplicates(timestamp_list)
-    # # IO.inspect Enum.count(timestamp_list)
-    # new_list = Enum.filter(timestamp_list, fn timestamp ->
-    #   ls = Enum.filter(list, fn x ->
-    #     if String.to_integer(x["unix_timestamp_100us"]) == timestamp do
-    #       x
-    #     end
-    #   end)
-    #   IO.inspect ls
-    # end)
-    # IO.inspect new_list
-
-
-    # publisher_pid = :global.whereis_name('publisher_pid')
-    # # GenServer.cast(publisher_pid, {:publish, [data, topic]})
-
-    # :timer.sleep(1000)
+    Enum.map(joined_list, fn joined_map ->
+      IO.inspect joined_map
+      GenServer.cast(publisher_pid, {:publish, [joined_map, topic]})
+    end)
   end
 
   defp add_to_list(list_iot, list_sensors, list_legacy_sensors, topic, map) do
@@ -99,22 +80,42 @@ defmodule JoinSensors do
     {list_iot, list_sensors, list_legacy_sensors}
   end
 
-  defp duplicates(list) do
-    acc_dupes = fn(x, {elems, dupes}) ->
-      case Map.has_key?(elems, x) do
-        true -> {elems, Map.put(dupes, x, nil)}
-        false -> {Map.put(elems, x, nil), dupes}
+  defp join_sensors(list_iot, list_legacy_sensors, list_sensors) do
+    joined_msg = Enum.map(list_iot, fn iot_msg ->
+      iot_timestamp = iot_msg[:unix_timestamp_100us] |> String.to_integer()
+
+      sensors_msg = Enum.find(list_sensors, fn sensors_msg ->
+        sensors_timestamp = sensors_msg[:unix_timestamp_100us] |> String.to_integer()
+        (iot_timestamp - sensors_timestamp <= 100) &&
+        (iot_timestamp - sensors_timestamp >= -100)
+      end)
+
+      legacy_sensors_msg = Enum.find(list_legacy_sensors, fn legacy_sensors_msg ->
+        legacy_sensors_timestamp = legacy_sensors_msg[:unix_timestamp_100us] |> String.to_integer()
+        (iot_timestamp - legacy_sensors_timestamp <= 100) &&
+        (iot_timestamp - legacy_sensors_timestamp >= -100)
+      end)
+
+      if sensors_msg != nil && legacy_sensors_msg != nil do
+        %{
+          atmo_pressure_sensor: iot_msg[:atmo_pressure_sensor],
+          wind_speed_sensor: iot_msg[:wind_speed_sensor],
+          humidity_sensor: legacy_sensors_msg[:humidity_sensor],
+          temperature_sensor: legacy_sensors_msg[:temperature_sensor],
+          light_sensor: sensors_msg[:light_sensor],
+          unix_timestamp_100us: iot_msg[:unix_timestamp_100us]
+        }
       end
-    end
+    end)
 
-    list |> Enum.reduce({%{}, %{}}, acc_dupes) |> elem(1) |> Map.keys()
-  end
+    #filter the nils
+    joined_msg = Enum.filter(joined_msg, fn x ->
+      if x != nil do
+        x
+      end
+    end)
 
-  defp get_duplicates(list, list_iot, list_legacy_sensors, list_sensors) do
-    #fuck
-
-
-    {list, list_iot, list_legacy_sensors, list_sensors}
+    {joined_msg, list_iot, list_legacy_sensors, list_sensors}
   end
 
   defp convert_string_to_map(str) do
